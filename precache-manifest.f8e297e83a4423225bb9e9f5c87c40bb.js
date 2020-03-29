c50f180a1f60db3ccb882ebf597565325480a2cf
Karolis Baukas <KarolisBaukas5@gmail.com>
2020-03-30 00:47:31 +0300

    Updates

---
+++ precache-manifest.f8e297e83a4423225bb9e9f5c87c40bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa5333fc8771b1b8f0a974541a90ff4",
+    "url": "/React-Loto/index.html"
+  },
+  {
+    "revision": "da10f7fc987e64abdf04",
+    "url": "/React-Loto/static/css/main.0e217263.chunk.css"
+  },
+  {
+    "revision": "c8a1c14191a997d5e15b",
+    "url": "/React-Loto/static/js/2.f0155fc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Loto/static/js/2.f0155fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da10f7fc987e64abdf04",
+    "url": "/React-Loto/static/js/main.22659727.chunk.js"
+  },
+  {
+    "revision": "0b23332ad08d25621af4",
+    "url": "/React-Loto/static/js/runtime-main.2b544b3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Loto/static/media/logo.5d5d9eef.svg"
+  }
+]);
